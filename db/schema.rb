0c24eda9f33707981401001a2013af1a7f1bc850
Mikhail Dyadchenko <mikhail_dyadchenko@mail.ru>
2024-10-30 05:50:29 +0300

    EmailFormAdded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_234855) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_30_024227) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -64,6 +64,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_29_234855) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
